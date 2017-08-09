953bc85f53cf602dc8d4c8c8b07cea2c0f97dd50
sammitjain <sammit.bitspilani@gmail.com>
2017-08-09 20:13:53 +0530

    Solved P1

--- prob1.m
+++ prob1.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % Problem 1, Lab Sheet 1 (Tank height)
 % Submitted by: Sammit Jain | 2014B4A3909G
 
@@ -7,14 +6,4 @@ function hdot = prob1(h,t)  %Function to initialize ODE
     beta = 2;
     
     hdot = alpha - beta*sqrt(h);    %Return final ODE
-=======
-% Problem 1, Lab Sheet 1 (Tank height)
-% Submitted by: Sammit Jain | 2014B4A3909G
-
-function hdot = prob1(h,t)  %Function to initialize ODE
-    alpha = 10 + 4*sin(t);  %Define other variables
-    beta = 2;
-    
-    hdot = alpha - beta*sqrt(h);    %Return final ODE
->>>>>>> 6d6d32346b194d934b7a787c27bc0dbbb49240cc
 end
--- prob1script.m
+++ prob1script.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 t_0 = 0;
 t_f = 30;
 
@@ -12,19 +11,4 @@ h0 = 1;                     %Supply Initial values
 
 [T,H] = ode45('prob1',tspan,h0);    %Return result
 
-=======
-t_0 = 0;
-t_f = 30;
-
-N = 60;
-
-step_size = (t_f-t_0)/N;    %Initialize Step Size
-
-tspan = t_0:step_size:t_f;  %Create spanning vector
-
-h0 = 1;                     %Supply Initial values
-
-[T,H] = ode45('prob1',tspan,h0);    %Return result
-
->>>>>>> 6d6d32346b194d934b7a787c27bc0dbbb49240cc
 plot(T,H)                   %Plot result
