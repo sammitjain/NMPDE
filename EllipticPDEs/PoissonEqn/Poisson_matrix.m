7ae52da224d8f9de4fd84a8a5b5a13a822a0cd1f
sammitjain <sammit.bitspilani@gmail.com>
2017-09-30 20:24:44 +0530

    Solved Poisson Eqn

--- EllipticPDEs/PoissonEqn/Poisson_matrix.m
+++ EllipticPDEs/PoissonEqn/Poisson_matrix.m
@@ -11,16 +11,20 @@
 clear all;
 close all;
 
+%Initializing the domain
+
 a = 0; 
 b = 1; 
 c = 0; 
 d = 1;
 
+%Setting up the resolution in x and y axes
 M = 40; 
 N = 40;
 
 format long;
 
+%Setting step size
 hx = (b-a)/M; 
 hx1 = hx*hx; 
 
@@ -40,26 +44,29 @@ for i=1:N-1,
   y(i) = i*hy;
 end
 
-
+%Finding the total number of linear elements
 M1 = (N-1)*(M-1); 
 
 A = sparse(M1,M1); 
 
 F = zeros(M1,1);
 
-for j = 1:N-1,
-  for i=1:M-1,
-    k = i + (j-1)*(M-1);
+for j = 1:N-1
+  for i=1:M-1
+    k = i + (j-1)*(M-1); %Linearizing the 2D domain
+    
     F(k) = fpoisson(x(i),y(j));
+    
     A(k,k) = -2/hx1 -2/hy1;
+    
     if i == 1
         A(k,k+1) = 1/hx1;
     else
        if i==M-1
          A(k,k-1) = 1/hx1;
        else
-	  A(k,k-1) = 1/hx1;
-	  A(k,k+1) = 1/hx1;
+        A(k,k-1) = 1/hx1;
+        A(k,k+1) = 1/hx1;
        end
     end
 
@@ -91,16 +98,17 @@ for k=1:M1
   j = fix(k/(M-1)) + 1;
 end
 
-% Analyze abd Visualize the result.
+% Analyze and Visualize the result.
 
 e = max( max( abs(Uapp-Uex)))        % The maximum error
-
+figure;
+subplot(1,2,1);
 surf(x,y,Uapp); 
 title('The Approximate solution plot'); 
 xlabel('x'); 
 ylabel('y');
 
-figure(2); 
+subplot(1,2,2);
 surf(x,y,Uex); 
 title('The Exact solution plot'); 
 xlabel('x');
